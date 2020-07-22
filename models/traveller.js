f9041a57a278977df2ab60c8b3b8fa65bcc09f61
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-22 16:26:05 +0100

    get journeys over certain distance test passes

--- models/traveller.js
+++ models/traveller.js
@@ -20,9 +20,10 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
   });
 };
 
-
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
